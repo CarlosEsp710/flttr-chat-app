7d6b9ac7f67b9c44c89e1dd4e8cf5a5a6826288f
Carlos Espejel <carlosespejel71@gamil.com>
2021-09-02 12:34:16 -0500

    Definimos las rutas y vistas de nuestra app

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RegsiterPage extends StatefulWidget {
+  const RegsiterPage({Key? key}) : super(key: key);
+
+  @override
+  _RegsiterPageState createState() => _RegsiterPageState();
+}
+
+class _RegsiterPageState extends State<RegsiterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Register Page'),
+      ),
+    );
+  }
+}
---
+++ lib/pages/users_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UsersPage extends StatefulWidget {
+  const UsersPage({Key? key}) : super(key: key);
+
+  @override
+  _UsersPageState createState() => _UsersPageState();
+}
+
+class _UsersPageState extends State<UsersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Users Page'),
+      ),
+    );
+  }
+}
